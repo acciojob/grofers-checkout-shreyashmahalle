548f144f9e2736496c23dbc64dfa3ec17afdee4c
shreyashmahalle <128777174+shreyashmahalle@users.noreply.github.com>
2025-09-22 10:23:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,8 +4,21 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+	let price = document.querySelectorAll(".price");
+	let total = 0;
+
+	price.forEach(p => {
+		total += Number(p.textContent);
+})
+	let row = document.createElement("tr");
+	let cell = document.createElement("td");
+	cell.setAttribute("colspan", "2");
+	cell.textContent = "total"  + total;
+
+	row.appendChild(cell);
+	document.querySelector("table").appendChild(row);
 };
+
 
 getSumBtn.addEventListener("click", getSum);
 
